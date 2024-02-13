72c70e0d8ffbafd64a5a06c2f2f0fe13d8fc7bb6
Mohammed Ezzaim <m.ezzaim2139@uca.ac>
2024-02-13 01:12:02 +0100

    succes

---
+++ coustomer-service/src/test/java/usa/harvard/coustomerservice/CoustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package usa.harvard.coustomerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoustomerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
