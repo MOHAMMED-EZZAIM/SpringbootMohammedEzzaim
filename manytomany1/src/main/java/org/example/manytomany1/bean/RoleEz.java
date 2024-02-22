14d670e886c0843411a65acb574ddc34536fbed9
Mohammed Ezzaim <m.ezzaim2139@uca.ac>
2024-02-22 08:46:31 +0100

    Succes!

---
+++ manytomany1/src/main/java/org/example/manytomany1/bean/RoleEz.java
@@ -0,0 +1,27 @@
+package org.example.manytomany1.bean;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class RoleEz {
+    @Id @GeneratedValue
+    private Long id;
+    private String description;
+    private String rolename;
+    @ManyToMany
+    private List<UserEz> userEzList;
+}
+
+
+
+
+
+
+
+
---
+++ manytomany1/src/test/java/org/example/manytomany1/Manytomany1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.manytomany1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Manytomany1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
